fa86e61a664758cfca0932b860cc8b106c9d8d15
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-06-26 13:15:12 +0200

    spring java config

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -4,10 +4,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pl.sda.config.ApplicationConfiguration;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
-
+        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
     }
 }
