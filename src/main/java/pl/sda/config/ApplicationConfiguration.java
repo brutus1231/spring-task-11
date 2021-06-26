548bdc14131ddf454967b233e51040571d9e9ed1
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-06-26 14:26:00 +0200

    spring properties

--- src/main/java/pl/sda/config/ApplicationConfiguration.java
+++ src/main/java/pl/sda/config/ApplicationConfiguration.java
@@ -1,13 +1,11 @@
 package pl.sda.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.CUSTOM,
         classes = ComponentScanCustomFilter.class), basePackages = {"pl.sda"})
+@PropertySource("classpath:db.properties")
 public class ApplicationConfiguration {
 
     @Bean
