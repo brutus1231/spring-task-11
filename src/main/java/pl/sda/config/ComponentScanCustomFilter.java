db2aba0e3e42ba806015221db96e10600ea08a21
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-06-26 14:04:02 +0200

    spring java config

--- src/main/java/pl/sda/config/ComponentScanCustomFilter.java
+++ src/main/java/pl/sda/config/ComponentScanCustomFilter.java
@@ -5,13 +5,11 @@ import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.core.type.classreading.MetadataReaderFactory;
 import org.springframework.core.type.filter.TypeFilter;
 
-import java.io.IOException;
-
 public class ComponentScanCustomFilter implements TypeFilter {
 
     @Override
     public boolean match(MetadataReader metadataReader,
-                         MetadataReaderFactory metadataReaderFactory) throws IOException {
+                         MetadataReaderFactory metadataReaderFactory) {
         ClassMetadata classMetadata = metadataReader.getClassMetadata();
         String fullyQualifiedName = classMetadata.getClassName();
         String className = fullyQualifiedName.substring(fullyQualifiedName.lastIndexOf(".") + 1);
