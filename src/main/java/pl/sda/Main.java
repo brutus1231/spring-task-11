e1f422fa1a07fc438d4a917e0c922761f5b16c2f
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-06-26 13:42:13 +0200

    spring java config

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -3,6 +3,7 @@ package pl.sda;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pl.sda.config.ApplicationConfiguration;
+import pl.sda.service.HelloWorldBo;
 
 import java.util.Arrays;
 
@@ -11,5 +12,8 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
         System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+
+        HelloWorldBo helloWorldBo = context.getBean(HelloWorldBo.class);
+        helloWorldBo.printMessage("Marek");
     }
 }
