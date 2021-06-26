818e890be5799a3a69e9f5669123f960e47cb321
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-06-26 14:33:27 +0200

    spring properties

--- src/main/java/pl/sda/service/HelloWorldBo.java
+++ src/main/java/pl/sda/service/HelloWorldBo.java
@@ -3,4 +3,6 @@ package pl.sda.service;
 public interface HelloWorldBo {
 
     void printMessage(String message);
+
+    void printProperty();
 }
--- src/main/java/pl/sda/service/HelloWorldBoImpl.java
+++ src/main/java/pl/sda/service/HelloWorldBoImpl.java
@@ -1,12 +1,21 @@
 package pl.sda.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloWorldBoImpl implements HelloWorldBo {
 
+    @Value("{db.username}")
+    private String dbUser;
+
     @Override
     public void printMessage(String message) {
         System.out.println("Hello world " + message);
+    }
+
+    @Override
+    public void printProperty() {
+        System.out.println(dbUser);
     }
 }
